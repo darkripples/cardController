d97224c32d76988576c5244d696dda8643b271c2
fls <fls@darkripples.com>
2019-11-05 14:24:29 +0800

    (#)测试通过明华RF32读卡器设备

--- cardControllerRF35.py
+++ cardControllerRF35.py
@@ -6,31 +6,44 @@ from ez_utils import read_conf
 confObj = read_conf("./conf/conf.ini")
 # 扇区号
 sectorNum = int(confObj.CONF.sectorNum)
-# 起始块号
-bStartBlockNumber = int(confObj.CONF.bStartBlockNumber)
 # 新密码
 newPwd = eval(confObj.CONF.newPwd)
 # dll文件
 dllPath = confObj.RF32.dllPath
 # 串口
-comNum = int(confObj.RF32.comNum)
+comNum = int(confObj.RF32.comNum) - 1
 # 波特率
 bps = int(confObj.RF32.bps)
 
-def main():
 
+def main():
     # 初始化
-    f = RF35Manage(r"D:\pythonWorkspaces\final\cardController\libs\mwrf32.dll")
+    # f = RF35Manage(r"D:\pythonWorkspaces\mwrf32\X86\2005, 4, 26, 1\mwrf32.dll")
+    f = RF35Manage(dllPath)
     # 连接com
     ret = f.connect(comNum=comNum, bps=bps)
     if ret < 0:
         return
-    f.getStatus()
+    # f.getStatus()
     f.loadKey(keyAB=0, sectorNum=sectorNum, pwdList=newPwd)
-    f.beep(msec=2000)
-    # f.authentication()
-    # f.read()
-    f.disconnect()
+    #f.loadKeyHex(keyAB=0, sectorNum=sectorNum, pwdList=newPwd)
+    # f.beep(msec=50)
+    try:
+        needRfCard = False
+        f.rfCard()
+        while 1:
+            if needRfCard:
+                f.rfCard()
+            f.authentication()
+            resp, cardCont = f.read(sectorNum=sectorNum + 4)
+            print(resp, cardCont)
+            f.halt()
+            needRfCard = True
+            break
+
+
+    finally:
+        f.disconnect()
 
 
 main()
--- tools/RF35Manage.py
+++ tools/RF35Manage.py
@@ -4,8 +4,7 @@ RF32非接触式读卡器utils
 """
 
 import os
-from ctypes import windll, c_ulong, c_byte
-from ez_utils import err_check
+from ctypes import windll, c_ulong, c_byte, c_int16, c_long, c_int, c_char, c_ubyte, c_short
 
 
 def consoleLog(*args):
@@ -26,7 +25,6 @@ class RF35Manage:
         else:
             consoleLog(self.logPre, "载入[", dllPath, "]失败，文件不存在")
 
-    @err_check
     def connect(self, comNum=2, bps=115200):
         """
         建立连接
@@ -36,9 +34,9 @@ class RF35Manage:
         """
         if not self.dllObj:
             return
-        resp = self.dllObj.rf_init(comNum, bps)
-        if resp > 0:
-            self.sessionId = resp
+        resp = self.dllObj.rf_init(c_int16(comNum), c_long(bps))
+        if resp and resp > 0:
+            self.sessionId = c_int(resp)
             consoleLog(self.logPre, "打开连接", resp)
         else:
             consoleLog(self.logPre, "连接com:", comNum, ",bps:", bps, ",失败：", resp)
@@ -51,9 +49,10 @@ class RF35Manage:
         """
         if not self.sessionId:
             return
-        out = (c_ulong * 32)(0)
+        out = (c_char * 20)(0)
         resp = self.dllObj.rf_get_status(self.sessionId, out)
-        consoleLog("状态信息:", resp)
+        consoleLog(self.logPre, "获取版本信息:", list(out))
+        return resp
 
     def loadKey(self, keyAB=0, sectorNum=1, pwdList=None):
         """
@@ -65,15 +64,65 @@ class RF35Manage:
         """
         if not self.sessionId:
             return
+        pwdBuffer = (c_ubyte * 6)(0x0)
+        if pwdList:
+            pwdBuffer = (c_ubyte * 6)(*pwdList)
+        resp = self.dllObj.rf_load_key(self.sessionId, keyAB, sectorNum, pwdBuffer)
+        if resp >= 0:
+            consoleLog(self.logPre, "加载密码成功")
+        else:
+            consoleLog(self.logPre, "加载密码失败:", resp, "|", str(list(pwdBuffer)))
+        return hex(resp)
+
+    def loadKeyHex(self, keyAB=0, sectorNum=1, pwdList=None):
+        """
+        加载密码,不与卡片通讯
+        :param keyAB: 0为keyA；4为keyB
+        :param sectorNum:
+        :param pwdList:
+        :return:
+        """
+        if not self.sessionId:
+            return
         pwdBuffer = (c_byte * 6)(0x0)
         if pwdList:
-            for i, nr in enumerate(pwdList):
-                pwdBuffer[i] = nr
-        resp = self.dllObj.rf_load_key(self.sessionId, keyAB, sectorNum, pwdBuffer)
-        if resp == 0:
-            consoleLog(self.logPre, "加载密码成功", resp)
+            pwdBuffer = (c_byte * 6)(*pwdList)
+        resp = self.dllObj.rf_load_key_hex(self.sessionId, keyAB, sectorNum, pwdBuffer)
+        if resp >= 0:
+            consoleLog(self.logPre, "加载密码成功:", resp)
         else:
-            consoleLog(self.logPre, "加载密码失败", resp)
+            consoleLog(self.logPre, "加载密码失败:", resp, "|", str(list(pwdBuffer)))
+        return hex(resp)
+
+    def rfCard(self):
+        """
+        寻卡
+        :return:
+        """
+        if not self.sessionId:
+            return
+        out = (c_ubyte * 5)(0x0)
+        # 0.只对1张卡操作
+        resp = self.dllObj.rf_card(self.sessionId, c_short(1), out)
+        # consoleLog(self.logPre, "寻卡输出:", list(out))
+        if resp >= 0:
+            consoleLog(self.logPre, "寻卡成功")
+        else:
+            consoleLog(self.logPre, "寻卡失败:", resp)
+        return hex(resp)
+
+    def halt(self):
+        """
+        终止对卡片的操作
+        :return:
+        """
+        if not self.sessionId:
+            return
+        resp = self.dllObj.rf_halt(self.sessionId)
+        # if resp == 0:
+        #     consoleLog(self.logPre, "终止卡操作成功")
+        # else:
+        #     consoleLog(self.logPre, "终止卡操作失败:", resp)
         return hex(resp)
 
     def authentication(self, keyAB=0, sectorNum=1):
@@ -86,23 +135,24 @@ class RF35Manage:
         if not self.sessionId:
             return
         resp = self.dllObj.rf_authentication(self.sessionId, keyAB, sectorNum)
-        if resp == 0:
-            consoleLog(self.logPre, "验证密码成功", resp)
+        if resp >= 0 and resp != 1:
+            consoleLog(self.logPre, "验证密码成功:", resp)
         else:
-            consoleLog(self.logPre, "验证密码失败", resp)
+            consoleLog(self.logPre, "验证密码失败:", resp)
         return hex(resp)
 
     def beep(self, msec=1000):
         """
         蜂鸣
-        :param msec: 毫秒数
+        :param msec: 10毫秒数
         :return:
         """
         if not self.sessionId:
             return
+        # msec单位是10毫秒
         resp = self.dllObj.rf_beep(self.sessionId, msec)
-        consoleLog("蜂鸣:", resp)
-        consoleLog(self.logPre, "蜂鸣时间:", msec, "毫秒")
+        if resp == 0:
+            consoleLog(self.logPre, "蜂鸣时间:", msec, "x10 毫秒")
 
     def read(self, sectorNum=1):
         """
@@ -112,11 +162,14 @@ class RF35Manage:
         """
         if not self.sessionId:
             return
-        out = (c_ulong * 32)(0)
-        resp = self.dllObj.rf_read(self.sessionId, c_byte(sectorNum), out)
-        consoleLog("读取:", resp)
-        consoleLog("读取:", list(out))
-        return list(out)
+        out = (c_ubyte * 32)(0)
+        resp = self.dllObj.rf_read(self.sessionId, c_int(sectorNum), out)
+        if resp >= 0 and resp != 1:
+            consoleLog(self.logPre, "读卡成功")
+        else:
+            out = []
+            consoleLog(self.logPre, "读卡失败:", resp)
+        return (resp, list(out))
 
     def readHex(self, sectorNum=1):
         """
@@ -126,10 +179,12 @@ class RF35Manage:
         """
         if not self.sessionId:
             return
-        out = (c_ulong * 32)(0)
-        resp = self.dllObj.rf_read_hex(self.sessionId, c_byte(sectorNum), out)
-        consoleLog("读取:", resp)
-        consoleLog("读取:", list(out))
+        out = (c_ubyte * 32)(0)
+        resp = self.dllObj.rf_read_hex(self.sessionId, c_int(sectorNum), out)
+        if resp == 0:
+            consoleLog(self.logPre, "hex读卡成功:", list(out))
+        else:
+            consoleLog(self.logPre, "hex读卡失败:", resp)
         return list(out)
 
     def disconnect(self):
@@ -140,29 +195,21 @@ class RF35Manage:
         if not self.sessionId:
             return
         resp = self.dllObj.rf_exit(self.sessionId)
-        consoleLog("关闭连接", resp)
+        consoleLog(self.logPre, "关闭连接", resp)
+        if resp == 0:
+            self.sessionId = None
 
 
 def main():
-    # 扇区号
-    sectorNum = 1
-    # 起始块号
-    bStartBlockNumber = 1
-    # 默认密码
-    defaultPwd = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-
-    # 初始化
-    f = RF35Manage(r"./mwrf32/X86/2005, 4, 26, 1/mwrf32.dll")
-    # 连接com
-    ret = f.connect(comNum=2, bps=115200)
-    if ret < 0:
-        return
-    f.getStatus()
-    f.loadKey(keyAB=0, sectorNum=sectorNum, pwdList=defaultPwd)
-    f.beep(msec=2000)
-    # f.authentication()
-    # f.read()
-    f.disconnect()
+    newPwd = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+    # #pwdBuffer = (c_byte * 6)(*newPwd)
+    # pwdBuffer = (c_ubyte * 6)(*newPwd)
+    # print(list(pwdBuffer))
+    # for i, nr in enumerate(newPwd):
+    #     pwdBuffer[i] = nr
+    # print(list(pwdBuffer))
+    pwdBuffer = (c_char * 6)(*newPwd)
+    print(list(pwdBuffer))
 
 
 if __name__ == "__main__":
